5ac374d286240676f9e52959de66afb36d56a910
Juan Ramirez <ramirjua94@gmail.com>
2020-04-03 22:42:57 -0700

    begin adding react front sanity check

---
+++ public/js/main.js
@@ -0,0 +1,16 @@
+// React
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+//class Square extends React.Component {
+//    render() {
+/*      return (
+        "Hello world"
+      );
+    }
+}*/
+
+ReactDOM.render(
+    <h1>hello world!</h1>,
+    document.getElementById('root')
+);
