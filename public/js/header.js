37aed9da7416928f94029a0d0b22cd600a912047
Juan Ramirez <ramirjua94@gmail.com>
2020-04-04 17:13:16 -0700

    abstract header

---
+++ public/js/header.js
@@ -0,0 +1,11 @@
+var React = require('react');
+
+function Header(props) {
+    return (
+      <div>
+          <button></button>
+          <button></button>
+          <button></button>
+      </div>
+    );
+  }
